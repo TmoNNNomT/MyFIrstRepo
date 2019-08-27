dac0adc509fdc199737f5f72129b059514ba1a80
Ark Shankar Pandey <hi.the.world.says.hello@gmail.com>
2019-08-27 17:24:13 +0530

    first change

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,6 @@ int main()
 	printf("Hello World!\n");
 	int num;
 	scanf("%d",&num);
-	printf("Did you just now enter %d ???", num);
+	printf("Did you just now enter %d ???", num + 1);
 	return 0;
 }
